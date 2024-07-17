9b79bb0871efe75ee5c47a45348f9a06f16324f0
PSYMURAI <kakadedhananjay59@gmail.com>
2024-07-17 13:28:30 +0530

    india

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.ts';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>
 );
 
